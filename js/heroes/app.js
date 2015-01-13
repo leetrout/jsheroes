127c2cc4ccdab9afc227b5a13aafce3a375b4d7a
Lee Trout <lee@leetrout.com>
2015-01-13 16:18:17 -0500

    Add angular directive and scope

--- js/heroes/app.js
+++ js/heroes/app.js
@@ -1,1 +1,13 @@
-console.log("I'm the heroes app");
+var heroes = angular.module('heroes', []);
+
+heroes.controller('HeroController', ['$scope', 
+  function($scope){
+    $scope.heroes = ['spiderman', 'batman'];
+  }
+]);
+
+heroes.directive('heroList', function(){
+  return {
+    templateUrl: 'frags/hero_list.html'
+  }
+});
