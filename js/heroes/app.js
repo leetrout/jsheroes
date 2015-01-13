abbf7dc30dcf93aeb1c86cf33034149efac77cac
Lee Trout <lee@leetrout.com>
2015-01-13 16:27:23 -0500

    Add hero addition

--- js/heroes/app.js
+++ js/heroes/app.js
@@ -3,11 +3,23 @@ var heroes = angular.module('heroes', []);
 heroes.controller('HeroController', ['$scope', 
   function($scope){
     $scope.heroes = ['spiderman', 'batman'];
+    $scope.addNewHero = function(hero) {
+      $scope.heroes.push(hero)
+    }
   }
 ]);
 
 heroes.directive('heroList', function(){
   return {
     templateUrl: 'frags/hero_list.html'
+  }
+});
+
+heroes.directive('heroCreator', function(){
+  return {
+    scope: {
+      addNewHero:"="
+    },
+    templateUrl: 'frags/hero_add.html'
   }
 });
